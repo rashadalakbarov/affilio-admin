5b6aef4f6172946d1e9b867fec3a542bee3b84eb
Rashad Alakbarov <rashad.alakbarov.777@gmail.com>
2025-05-29 23:44:00 +0400

    create dashboard and index page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\admin\AuthController;
+use App\Http\Controllers\admin\DashboardController;
+
+Route::name('admin.')->controller(AuthController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
